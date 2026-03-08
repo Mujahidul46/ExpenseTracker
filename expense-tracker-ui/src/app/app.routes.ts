2aacf109d86e62fec2941510f64e958d7d32240b
Mujahidul Islam <mujahidul46@outlook.com>
2026-03-08 18:52:18 +0000

    delete appointments

--- expense-tracker-ui/src/app/app.routes.ts
+++ expense-tracker-ui/src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { UserDashboardComponent } from './components/user-dashboard/user-dashboa
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard';
 import { LoginComponent  } from './components/login/login';
 import { SignupComponent } from './components/signup/signup';
-import { AppointmentsComponent } from './components/appointments/appointments';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'signup', component: SignupComponent},
     { path: 'user-dashboard', component: UserDashboardComponent },
-    { path: 'appointments', component: AppointmentsComponent },
     { path: 'admin-dashboard', component: AdminDashboardComponent },
 ];
